7637587bfc2b0385db37dd24e01d51bb8b76cf98
edeforas <etienne.deforas@gmail.com>
2020-01-05 20:04:28 +0100

    updated test

--- src/tests/test_layer_forward.cpp
+++ src/tests/test_layer_forward.cpp
@@ -9,20 +9,24 @@ void test_layer_conv2d()
 	MatrixFloat mIn, mOut, mKernel;
 	mIn.setZero(5, 5);
 	mIn(2, 2) = 1;
+	mIn.resize(1, 5 * 5);
 
 	mKernel.setZero(3, 3);
 	mKernel(1, 0) = 1;
 	mKernel(1, 1) = 2;
 	mKernel(1, 2) = 1;
+	mKernel.resize(1, 3 * 3);
 	
 	LayerConvolution2D conv2d(5,5,1,3,3,1);
 	conv2d.weights() = mKernel;
 	conv2d.forward(mIn, mOut);
 
+	mOut.resize(3, 3);
 	cout << toString(mOut) << endl;
 }
-
+/////////////////////////////////////////////////////////////////
 int main()
 {
 	test_layer_conv2d();
 }
+/////////////////////////////////////////////////////////////////
