9f050cad26f6dbe7c367c8587974f1dce7ef8b5d
edeforas <etienne.deforas@gmail.com>
2020-07-18 19:10:35 +0200

    added pytest test

---
+++ python/test_sinus_fit.py
@@ -0,0 +1,32 @@
+import numpy as np
+import BeeDNN as nn
+
+# Simple Sinus regression (fit) using small network
+# network is very small so we can see the fitting error
+# this testis to be used with pytest 
+
+def test_fit():
+	# create train data
+	sample = np.arange(-4.,4.,0.01)[:,np.newaxis]
+	truth = np.sin(sample)
+
+	# construct net
+	n = nn.Net()
+	n.set_classification_mode(False) #because we do regression here
+	n.append(nn.LayerDense(1,20))
+	n.append(nn.LayerRELU())
+	n.append(nn.LayerDense(20,1))
+
+	# train net
+	train = nn.NetTrain()
+	train.epochs = 100
+	train.batch_size=32
+	train.set_optimizer(nn.opt.OptimizerNadam())
+	train.set_loss(nn.LossMSE()) # simple Mean Square Error
+	train.train(n,sample,truth)
+
+	# test convergence
+	x = sample
+	y = n.forward(x)
+	dist=np.abs(y-truth);
+	assert np.max(dist)<0.1
---
+++ python/test_xor_classification.py
@@ -0,0 +1,31 @@
+import numpy as np
+import BeeDNN as nn
+
+#simple xor classification
+def test_xor_classification():
+	# create train data
+	sample=np.zeros((4,2))
+	sample[0,0]=0 ; sample[1,0]=0 ; sample[2,0]=1 ; sample[3,0]=1
+	sample[0,1]=0 ; sample[1,1]=1 ; sample[2,1]=0 ; sample[3,1]=1
+	truth=np.zeros((4,1))
+	truth[0,0]=0 ; truth[1,0]=1  ; truth[2,0]=1 ; truth[3,0]=0
+
+	# construct net
+	n = nn.Net()
+	n.append(nn.LayerDense(2,3))
+	n.append(nn.LayerTanh())
+	n.append(nn.LayerDense(3,1))
+
+	# optimize net
+	train = nn.NetTrain()
+	train.epochs = 100
+	train.batch_size=0 #if set to 0, use full batch
+	train.set_optimizer(nn.opt.OptimizerRPROPm())  # work best with full batch size
+	train.set_loss(nn.LossMSE()) # simple Mean Square Error
+	train.train(n,sample,truth)
+
+	# test convergence
+	x = sample
+	y = n.forward(x)
+	dist=np.abs(y-truth);
+	assert np.max(dist)<0.001
